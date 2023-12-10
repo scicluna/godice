0ab3e7c66a6e063839ef80bcae9eea18b5595038
scicluna <88948700+scicluna@users.noreply.github.com>
2023-12-10 11:45:54 -0500

    parsing strings

--- roller/roller.go
+++ roller/roller.go
@@ -1,30 +1,19 @@
 package roller
 
 import (
-	"errors"
 	"math/rand"
-	"strconv"
-	"strings"
+	"regexp"
 )
 
-//parse the roll string (2d6) and then process the roll
-
-func RollDice(roll string) (int, error) {
-	var result int
-	parsedRoll := strings.Split(roll, "d")
-	quantity, errQuantity := strconv.Atoi(parsedRoll[0])
-	sides, errSides := strconv.Atoi(parsedRoll[1])
-
-	if errQuantity != nil || errSides != nil {
-		return 0, errors.New("Invalid roll")
-	}
-
-	for i := 0; i < quantity; i++ {
-		result += Roll(sides)
-	}
-	return result, nil
+// given the size of a die, roll a single die and return the result
+func singleRoll(size int) int {
+	return rand.Intn(size) + 1
 }
 
-func Roll(sides int) int {
-	return rand.Intn(sides) + 1
+// split a roll string into useful parts (array of dice sizes, array of dice quantities, array of modifiers, array of operands)
+func parseRollString(rollString string) ([]int, []int, []string, []string, error) {
+	reOperands := regexp.MustCompile(`([+-*/])`)
+	dicePairs := reOperands.Split(rollString, -1)
+	operands := reOperands.FindAllString(rollString, -1)
+
 }
